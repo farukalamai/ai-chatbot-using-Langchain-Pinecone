b48708c019e2ea153404cbd32a4c4a43c33d3a77
Md Faruk Alam <92469073+farukalampro@users.noreply.github.com>
2023-07-09 14:36:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", 
-openai_api_key="")
+openai_api_key="" ## find at platform.openai.com)
 
 if 'buffer_memory' not in st.session_state:
             st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
